e49cbb0a4871f8101b323fd752af769341ee1168
渡辺のぞみ <@10.41.144.28>
2025-02-19 22:03:42 +0900

    java4時間目完了

--- src/EnjoyPractice.java
+++ src/EnjoyPractice.java
@@ -3,8 +3,11 @@ public class EnjoyPractice {
 
 	public static void main(String[] args) {
 		// EnjoyPractice! の出力
-		System.out.println("例外が\r\n発生しました。");
+		boolean boo = true;
+		System.out.println(boo);
 
 	}
 
 }
+
+	
