6cee41ce26c40ff3e8e4265bcd0eb367d3228ce3
渡辺のぞみ <@10.41.144.28>
2025-02-19 21:43:46 +0900

    java3時間目完了

--- src/EnjoyPractice.java
+++ src/EnjoyPractice.java
@@ -3,7 +3,7 @@ public class EnjoyPractice {
 
 	public static void main(String[] args) {
 		// EnjoyPractice! の出力
-		System.out.println("Enjoy Practice!");
+		System.out.println("例外が\r\n発生しました。");
 
 	}
 
