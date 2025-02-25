0fe712efc62567e9193d2ab23db533226985f626
渡辺のぞみ <@LAPTOP-Q9D017UT>
2025-02-25 21:09:58 +0900

    java8時間目完了　※{}忘れないように

--- src/EnjoyPractice.java
+++ src/EnjoyPractice.java
@@ -3,15 +3,21 @@ public class EnjoyPractice {
 
 	public static void main(String[] args) {
 		// EnjoyPractice! の出力
-		int a = 5;
+		int age = 25;
 		
-		if(a % 2 == 0 ) {
-			System.out.println("偶数");
-		}else {
-			System.out.println("奇数");
+		if(age <= 100) {
+			if(age < 20) {
+				System.out.println("未成年");
+			}else if(age == 77) {
+				System.out.println("喜寿");
+			}else if(age == 88) {
+				System.out.println("米寿");
+			}else{
+				System.out.println("成人");
 		}
 	}
 
+}
 }
 
 	
