5e9012ad5e75a17bd7f5735cc318f4d77c0105ad
渡辺のぞみ <@LAPTOP-Q9D017UT>
2025-02-25 21:51:35 +0900

    java12時間目完了　※不等号の向き注意！

--- src/EnjoyPractice.java
+++ src/EnjoyPractice.java
@@ -3,15 +3,9 @@ public class EnjoyPractice {
 
 	public static void main(String[] args) {
 		// EnjoyPractice! の出力
-		String[][]country = new String[3][2];
-		country[0][0] = "日本";
-		country[0][1] = "タイ";
-		country[1][0] = "アメリカ";
-		country[1][1] = "ブラジル";
-		country[2][0] = "フランス";
-		country[2][1] = "ロシア";
-				
-		System.out.println(country[1][1]);
+		for(int i = 1; i <= 5; i++) {
+			System.out.println("★");
+		}
 		}
 	}
 
