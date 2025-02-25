376269ea90599cc6b8ff7bdad0cf17f18ebce05f
渡辺のぞみ <@LAPTOP-Q9D017UT>
2025-02-25 21:43:44 +0900

    java11時間目完了

--- src/EnjoyPractice.java
+++ src/EnjoyPractice.java
@@ -3,13 +3,18 @@ public class EnjoyPractice {
 
 	public static void main(String[] args) {
 		// EnjoyPractice! の出力
-		String[]name = {"田中","高橋","斉藤"};
-		
-		name[0] = "加藤";
-		
-		System.out.println(name[0]);
+		String[][]country = new String[3][2];
+		country[0][0] = "日本";
+		country[0][1] = "タイ";
+		country[1][0] = "アメリカ";
+		country[1][1] = "ブラジル";
+		country[2][0] = "フランス";
+		country[2][1] = "ロシア";
+				
+		System.out.println(country[1][1]);
+		}
 	}
-}
+
 
 
 	
