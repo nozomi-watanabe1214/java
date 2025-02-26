f3f700cb8c1f2ab4313f890b2745beb88dd265e8
渡辺のぞみ <@192.168.0.15>
2025-02-26 21:46:29 +0900

    java13時間目完了

--- src/EnjoyPractice.java
+++ src/EnjoyPractice.java
@@ -3,9 +3,10 @@ public class EnjoyPractice {
 
 	public static void main(String[] args) {
 		// EnjoyPractice! の出力
-		for(int i = 1; i <= 5; i++) {
-			System.out.println("★");
-		}
+		for(int i = 0; i <=5; i++) {
+		String b = i % 2 == 0? "☆":"★";
+		System.out.println(b);
+			}
 		}
 	}
 
