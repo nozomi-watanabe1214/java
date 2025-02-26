1d503faf6dba9d094adf8a013d444bf61a319a2a
渡辺のぞみ <@192.168.0.15>
2025-02-26 21:55:08 +0900

    java14時間目完了　※★には""をつける！

--- src/EnjoyPractice.java
+++ src/EnjoyPractice.java
@@ -3,9 +3,10 @@ public class EnjoyPractice {
 
 	public static void main(String[] args) {
 		// EnjoyPractice! の出力
-		for(int i = 0; i <=5; i++) {
-		String b = i % 2 == 0? "☆":"★";
-		System.out.println(b);
+		int i = 1;
+		while(i <= 5) {
+			System.out.println("★");
+			i ++;
 			}
 		}
 	}
