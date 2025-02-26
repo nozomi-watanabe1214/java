952afb0f923bd16ff084f89f4b133401f9360bc8
渡辺のぞみ <@192.168.0.15>
2025-02-26 22:40:54 +0900

    java16時間目途中　※エラーあり

--- src/EnjoyPractice.java
+++ src/EnjoyPractice.java
@@ -3,11 +3,14 @@ public class EnjoyPractice {
 
 	public static void main(String[] args) {
 		// EnjoyPractice! の出力
-		int i = 10;
-		do {
-			System.out.println(i);
-			i -= 3;
-			} while(i >= 1);
+		public static String hello() {
+			return "こんにちは";
+			}
+		System.out.println("山田さん。"+hello());
+		System.out.println("<br>");
+		System.out.println("鈴木さん。"+hello());
+		System.out.println("<br>");
+		System.out.println("佐藤さん。"+hello());		
 		}
 	}
 
