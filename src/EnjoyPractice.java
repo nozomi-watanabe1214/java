552456c2307dac733985c447bbba3fbc4c22eccf
渡辺のぞみ <@192.168.0.15>
2025-03-01 00:28:32 +0900

    java16時間目完了

--- src/EnjoyPractice.java
+++ src/EnjoyPractice.java
@@ -1,17 +1,23 @@
 
 public class EnjoyPractice {
 
-	public static void main(String[] args) {
+	public static void main(String [] args) {
 		// EnjoyPractice! の出力
-		public static String hello() {
-			return "こんにちは";
-			}
-		System.out.println("山田さん。"+hello());
-		System.out.println("<br>");
-		System.out.println("鈴木さん。"+hello());
-		System.out.println("<br>");
-		System.out.println("佐藤さん。"+hello());		
+		
+		public static string hello() {
+			return "こんにちは" ;
 		}
+		
+		System.out.println("山田さん。"+ hello() );
+		System.out.println("<br>");
+		System.out.println("鈴木さん。"+ hello() );
+		System.out.println("<br>");
+		System.out.println("佐藤さん。"+ hello() );
+		
+		
+		
+		}
+	
 	}
 
 
