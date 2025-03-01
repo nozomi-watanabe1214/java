91e8da3e21d5fe6e2cd2ac2ee1b3b71513da53ba
渡辺のぞみ <@10.18.48.165>
2025-03-01 15:52:46 +0900

    コレクションフレームワーク写経①

--- src/jp/co/diworks/ListSample.java
+++ src/jp/co/diworks/ListSample.java
@@ -9,5 +9,14 @@ public class ListSample {
 		List<String>list = new ArrayList<String>();
 		
 		//値の記憶はaddメソッドを利用
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+		
+		//値の取得
+		
+		
 	}
 }
